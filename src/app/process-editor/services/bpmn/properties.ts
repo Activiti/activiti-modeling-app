2de6abe10e47af52dbec618c00ee8fa9b9135e19
georgiana-roman <georgiana.roman@ness.com>
2018-10-26 16:30:04 +0300

    apm-518 change callActivity name

--- src/app/process-editor/services/bpmn/properties.ts
+++ src/app/process-editor/services/bpmn/properties.ts
@@ -10,7 +10,7 @@ export enum BpmnProperty {
     candidateGroups = 'candidateGroups',
     dueDate = 'dueDate',
     priority = 'priority',
-    activityName = 'activityName',
+    activityName = 'calledElemet',
     properties = 'properties',
     conditionExpression = 'conditionExpression'
 }
