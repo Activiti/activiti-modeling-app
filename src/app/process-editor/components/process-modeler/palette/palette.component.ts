21d8dc4f1a5384f5c375b4faa12160e9f9726197
Andras Popovics <popovics@ndras.hu>
2019-03-21 12:58:47 +0100

    Fix typing issues

--- src/app/process-editor/components/process-modeler/palette/palette.component.ts
+++ src/app/process-editor/components/process-modeler/palette/palette.component.ts
@@ -17,7 +17,14 @@
 
 import { Component, Inject, Optional, HostListener, ViewChild, TemplateRef } from '@angular/core';
 import { ProcessModelerPaletteService } from '../../../services/palette/process-modeler-palette.service';
-import { PaletteElement, PaletteElementsToken, ToolTrigger, BpmnTrigger } from 'ama-sdk';
+import {
+    PaletteElement,
+    PaletteElementsToken,
+    ToolTrigger,
+    BpmnTrigger,
+    PaletteGroupElement,
+    PaletteSeparatorElement
+} from 'ama-sdk';
 import { OverlayRef } from '@angular/cdk/overlay';
 
 @Component({
@@ -45,11 +52,11 @@ export class PaletteComponent {
         this.paletteElements = (<any>paletteElements).flatten(1) || [];
     }
 
-    public isSeparator(element: PaletteElement) {
+    public isSeparator(element: PaletteSeparatorElement) {
         return element.group === 'separator';
     }
 
-    public hasChildren(element: PaletteElement) {
+    public hasChildren(element: PaletteGroupElement) {
         return element.group === 'container' && element.children && element.children.length;
     }
 
