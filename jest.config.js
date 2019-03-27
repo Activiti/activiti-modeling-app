c207bce55680d9912d6e1cd8430ceab48b6d3874
Alex Bolboșenco <alex.bolbosenco@gmail.com>
2019-03-27 14:43:16 +0200

    [APM-843] fix prod build

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "collectCoverageFrom": ["src/**/*.ts", "projects/**/*.ts", "!jest", "!src/*.ts", "!src/**/*.d.ts", "!src/**/index.ts"],
     "coverageDirectory": "./coverage/",
     "collectCoverage": false,
-    "moduleNameMapper": {},
+    "moduleNameMapper": {
+        'moddle': '<rootDir>/node_modules/moddle-transpiled',
+        'moddle-xml': '<rootDir>/node_modules/moddle-xml-transpiled',
+        'bpmn-moddle': '<rootDir>/node_modules/bpmn-moddle-transpiled',
+        'bpmn-js': '<rootDir>/node_modules/bpmn-js-transpiled',
+        'diagram-js': '<rootDir>/node_modules/diagram-js-transpiled'
+    },
     "transformIgnorePatterns": [
         "node_modules/(?!bpmn-moddle|@alfresco\\/js-api)"
     ],
--- jest/transpile-es2015-modules-for-jest.js
+++ jest/transpile-es2015-modules-for-jest.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
+function copyFolderSync(from, to) {
+  fs.mkdirSync(to);
+  fs.readdirSync(from).forEach(element => {
+      if (fs.lstatSync(path.join(from, element)).isFile()) {
+          fs.copyFileSync(path.join(from, element), path.join(to, element));
+      } else {
+          copyFolderSync(path.join(from, element), path.join(to, element));
+      }
+  });
+}
+
 // Jest doesn't want to behave with the transformIgnorePatterns, that is why this hack
 function transformFiles(workPath) {
-  console.log('Transforming: ' + workPath);
-
   const files = fs.readdirSync(workPath);
   for (let file of files) {
     const filePath = path.join(workPath, file);
@@ -39,7 +48,9 @@ const dirs = [
 for (let dir of dirs) {
   const workPath = __dirname + '/../node_modules/' + dir;
   if (fs.existsSync(workPath)) {
-    transformFiles(workPath);
+    console.log('Transforming: ' + workPath);
+    copyFolderSync(workPath, workPath + '-transpiled');
+    transformFiles(workPath + '-transpiled');
   } else {
       console.log(`${workPath} does not exists.`);
   }
