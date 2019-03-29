5ee27d8a5605a3445b249fc3718bb52f71d66d9a
georgiana-roman <georgiana.roman@ness.com>
2019-03-29 17:06:04 +0200

    apm-856 try to fix e2e

--- e2e/tests/login-logout/authorization.e2e.ts
+++ e2e/tests/login-logout/authorization.e2e.ts
@@ -25,18 +25,16 @@ describe('User Authorization', () => {
 
     let loginPage: LoginPageImplementation;
 
-    beforeEach(async () => {
+    it('1. [C289335] Login with user with "ACTIVITI_MODELER" role', async () => {
         loginPage = LoginPage.get(testConfig);
         await loginPage.navigateTo();
-    });
-
-    it('1. [C289335] Login with user with "ACTIVITI_MODELER" role', async () => {
         await loginPage.login(testConfig.ama.user, testConfig.ama.password);
-        expect(await authenticatedPage.isLoggedIn()).toBe(true);
-        await authenticatedPage.logout();
+        await authenticatedPage.isLoggedIn();
     });
 
     it('2. [C289854] Login with user without "ACTIVITI_MODELER" role ', async () => {
+        loginPage = LoginPage.get(testConfig);
+        await loginPage.navigateTo();
         await loginPage.login(testConfig.ama.unauthorized_user, testConfig.ama.unauthorized_user_password);
         expect(await loginPage.isLoginPageDisplayed()).toBe(true);
     });
