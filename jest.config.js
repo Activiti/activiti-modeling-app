cab6a673f9b50c2a4d8132b52f5cd4e262d3ca63
Andras Popovics <popovics@ndras.hu>
2019-04-02 18:57:02 +0200

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,19 @@ module.exports = {
     "coverageDirectory": "./coverage/",
     "collectCoverage": false,
     "moduleNameMapper": {
-        'moddle': '<rootDir>/node_modules/moddle-transpiled',
-        'moddle-xml': '<rootDir>/node_modules/moddle-xml-transpiled',
-        'bpmn-moddle': '<rootDir>/node_modules/bpmn-moddle-transpiled',
-        'bpmn-js': '<rootDir>/node_modules/bpmn-js-transpiled',
-        'diagram-js': '<rootDir>/node_modules/diagram-js-transpiled'
+        '^moddle$': '<rootDir>/node_modules/moddle-transpiled',
+        '^moddle/(.*)$': '<rootDir>/node_modules/moddle-transpiled/$1',
+        '^moddle-xml$': '<rootDir>/node_modules/moddle-xml-transpiled',
+        '^moddle-xml/(.*)$': '<rootDir>/node_modules/moddle-xml-transpiled/$1',
+        '^bpmn-moddle$': '<rootDir>/node_modules/bpmn-moddle-transpiled',
+        '^bpmn-moddle/(.*)$': '<rootDir>/node_modules/bpmn-moddle-transpiled/$1',
+        '^bpmn-js$': '<rootDir>/node_modules/bpmn-js-transpiled',
+        '^bpmn-js/(.*)$': '<rootDir>/node_modules/bpmn-js-transpiled/$1',
+        '^diagram-js$': '<rootDir>/node_modules/diagram-js-transpiled',
+        '^diagram-js/(.*)$': '<rootDir>/node_modules/diagram-js-transpiled/$1'
     },
     "transformIgnorePatterns": [
-        "node_modules/(?!bpmn-moddle|@alfresco\\/js-api)"
+        "node_modules/(?!@alfresco\\/js-api)"
     ],
     "setupFiles": ["core-js/es7/array"],
     "transform": {
