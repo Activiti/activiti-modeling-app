15619c1bdc9df9c321ed5c392783231241224cab
Georgiana Roman <georgiana.roman@ness.com>
2019-05-08 15:02:17 +0300

    [APM-963] change priorities to numbers (#141)
    * APM-963 change priority to int

--- src/app/process-editor/services/bpmn-js/property-handlers/priority.handler.ts
+++ src/app/process-editor/services/bpmn-js/property-handlers/priority.handler.ts
@@ -22,7 +22,7 @@ const propertyKey = BpmnProperty.priority;
 const get = element => element.businessObject.get(propertyKey);
 const set = (modeling: Bpmn.Modeling, element: Bpmn.DiagramElement, value: any) => {
     modeling.updateProperties(element, {
-        [propertyKey]: value
+        [propertyKey]: parseInt(value, 10)
     });
 };
 
