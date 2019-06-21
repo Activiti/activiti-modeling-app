a07855c2ec5805cb19981884fe07f2fb00525d17
Alex Bolboșenco <alex.bolbosenco@gmail.com>
2019-06-21 15:37:55 +0300

    [APM-1023] fix

--- e2e/test.config.ts
+++ e2e/test.config.ts
@@ -16,7 +16,7 @@
  */
 
 import * as appConfigJson from '../src/app.config.json';
-import { getConfig } from '../../alfresco-modeler-app/e2e/tests/project/node_modules/ama-testing/e2e';
+import { getConfig } from 'ama-testing/e2e';
 const apsConfigJson = <any>appConfigJson;
 
 export const testConfig = getConfig(__dirname, apsConfigJson);
