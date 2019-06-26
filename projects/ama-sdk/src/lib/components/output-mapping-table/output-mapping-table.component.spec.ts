58c8bc1bf3bdd50c860ccd6c36e913a64897cf3b
Denys Vuika <denys.vuika@alfresco.com>
2019-06-26 13:54:51 +0100

    [APM-1109] Move mapping editor to SDK to make it reusable (#197)
    * input mapping table component
    
    * output mapping table component
    
    * [APM-1117] fix layout issue for static input
    
    * export output table classes

---
+++ projects/ama-sdk/src/lib/components/output-mapping-table/output-mapping-table.component.spec.ts
@@ -0,0 +1,176 @@
+ /*!
+ * @license
+ * Copyright 2019 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableModule, MatTooltipModule, MatSelectModule, MatFormFieldModule } from '@angular/material';
+import { TranslateModule } from '@ngx-translate/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MappingType } from '../../api/types';
+import { OutputMappingTableComponent } from './output-mapping-table.component';
+
+describe('OutputMappingTableComponent', () => {
+    let fixture: ComponentFixture<OutputMappingTableComponent>;
+    let component: OutputMappingTableComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MatTableModule,
+                TranslateModule.forRoot(),
+                MatTooltipModule,
+                MatSelectModule,
+                MatFormFieldModule,
+                NoopAnimationsModule,
+                FormsModule
+            ],
+            declarations: [
+                OutputMappingTableComponent
+            ],
+            providers: [],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(OutputMappingTableComponent);
+        component = fixture.componentInstance;
+        component.parameters = [{
+            id: 'id1',
+            name: 'name',
+            description: 'desc',
+            type: 'string'
+        }];
+        component.processProperties = [
+            {
+                id: '1',
+                name: 'var1',
+                type: 'string',
+                required: false,
+                value: ''
+            },
+            {
+                id: '2',
+                name: 'var2',
+                type: 'date',
+                required: false,
+                value: ''
+            },
+            {
+                id: 'stringVarId',
+                name: 'stringVar',
+                type: 'string',
+                required: false,
+                value: ''
+            }
+        ];
+        component.mapping = {};
+
+        component.ngOnChanges();
+        fixture.detectChanges();
+    });
+
+    it('should render component', () => {
+        expect(component).not.toBeNull();
+    });
+
+    it('should emit the correct data when a property value is set', () => {
+        spyOn(component.update, 'emit');
+        const select = fixture.debugElement.query(By.css('.mat-select-trigger'));
+        select.nativeElement.click();
+        fixture.detectChanges();
+
+        const options = fixture.debugElement.queryAll(By.css('.mat-option'));
+        options[0].nativeElement.click();
+        fixture.detectChanges();
+
+        const data = { [component.processProperties[0].name]: {
+            type: MappingType.variable,
+            value: component.parameters[0].name
+        }};
+        expect(component.update.emit).toHaveBeenCalledWith(data);
+    });
+
+    it('should emit the correct data when a property value is reset', () => {
+        spyOn(component.update, 'emit');
+        component.mapping = { [component.processProperties[0].name]: {
+            type: MappingType.variable,
+            value: component.parameters[0].name
+        }};
+        component.ngOnChanges();
+
+        const select = fixture.debugElement.query(By.css('.mat-select-trigger'));
+        select.nativeElement.click();
+        fixture.detectChanges();
+        const options = fixture.debugElement.queryAll(By.css('.mat-option'));
+        options[1].nativeElement.click();
+        fixture.detectChanges();
+
+        const data = { [component.processProperties[2].name]: {
+            type: MappingType.variable,
+            value: component.parameters[0].name
+        }};
+
+        expect(component.update.emit).toHaveBeenCalledWith(data);
+    });
+
+    it('should filter the processProperties', () => {
+        const select = fixture.debugElement.query(By.css('.mat-select-trigger'));
+        select.nativeElement.click();
+        fixture.detectChanges();
+
+        const options = fixture.debugElement.queryAll(By.css('.mat-option'));
+        expect(options.length).toBe(2, 'Two options are expected from type: string');
+    });
+
+    it('should display a message if no process property', () => {
+        component.parameters[0].type = 'boolean';
+        component.ngOnChanges();
+        fixture.detectChanges();
+
+        const select = fixture.debugElement.query(By.css('.mat-select'));
+        const noPropMsg = fixture.debugElement.query(By.css('.no-process-properties-msg'));
+
+        expect(select).toBeNull();
+        expect(noPropMsg).not.toBeNull();
+    });
+
+    it('if a parameter is marked non-required then None value will appear first in the properties list to choose from', () => {
+        component.parameters.push({id: 'id2', name: 'name2', type: 'string', required: false});
+        component.ngOnChanges();
+        fixture.detectChanges();
+
+        expect(component.optionsForParams['name2'][0]).toEqual({id: null, name: 'None'});
+    });
+
+    it('if a parameter is marked required or not marked at all then None value will appear first in the properties list to choose from', () => {
+        component.parameters.push({id: 'id3', name: 'name3', type: 'string', required: true});
+        component.ngOnChanges();
+        fixture.detectChanges();
+
+        expect(component.optionsForParams['name']).toEqual([
+            component.processProperties[0],
+            component.processProperties[2]
+        ]);
+        expect(component.optionsForParams['name3']).toEqual([
+            component.processProperties[0],
+            component.processProperties[2]
+        ]);
+    });
+});
---
+++ projects/ama-sdk/src/lib/components/output-mapping-table/output-mapping-table.component.ts
@@ -0,0 +1,105 @@
+ /*!
+ * @license
+ * Copyright 2019 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { ConnectorParameter, EntityProperty, MappingType, ServiceOutputParameterMapping, ServiceParameterMappings } from '../../api/types';
+
+@Component({
+    selector: 'amasdk-output-mapping-table',
+    templateUrl: 'output-mapping-table.component.html',
+    host: { class: 'amasdk-input-mapping-table' }
+})
+export class OutputMappingTableComponent implements OnChanges {
+    @Input()
+    parameters: ConnectorParameter[];
+
+    @Input()
+    processProperties: EntityProperty[];
+
+    @Input()
+    mapping: ServiceOutputParameterMapping;
+
+    @Output()
+    update = new EventEmitter<ServiceParameterMappings>();
+    data: ServiceOutputParameterMapping = {};
+
+    displayedColumns: string[] = ['name', 'process-variable'];
+    dataSource: MatTableDataSource<ConnectorParameter>;
+    optionsForParams: {
+        [paramName: string]: { id: string; name: string }[];
+    } = {};
+    paramName2VariableName: { [paramName: string]: string } = {};
+
+    ngOnChanges() {
+        this.initOptionsForParams();
+        this.initMapping();
+        this.dataSource = new MatTableDataSource(this.parameters);
+        this.data = { ...this.mapping };
+    }
+
+    changeSelection(
+        { value: variableName },
+        parameter: ConnectorParameter
+    ): void {
+        const previousVariableMapped = this.paramName2VariableName[
+            parameter.name
+        ];
+        if (previousVariableMapped) {
+            delete this.data[previousVariableMapped];
+        }
+
+        this.data[variableName] = {
+            type: MappingType.variable,
+            value: parameter.name
+        };
+        this.update.emit(this.data);
+    }
+
+    clearSelection(parameterName) {
+        delete this.data[this.paramName2VariableName[parameterName]];
+        delete this.paramName2VariableName[parameterName];
+        this.update.emit(this.data);
+    }
+
+    initOptionsForParams() {
+        this.parameters.forEach(this.setOptionForAParam.bind(this));
+    }
+
+    private setOptionForAParam(param) {
+        this.optionsForParams[param.name] = [
+            ...(param.required === false ? [{ id: null, name: 'None' }] : []),
+            ...this.processProperties
+                .filter(variable => variable.type === param.type)
+                .filter(
+                    variable =>
+                        !this.mapping[variable.name] ||
+                        this.mapping[variable.name].value === param.name
+                )
+        ];
+    }
+
+    initMapping() {
+        const variableNames = Object.keys(this.mapping);
+
+        for (const variableName of variableNames) {
+            this.paramName2VariableName[
+                this.mapping[variableName].value
+            ] = variableName;
+        }
+    }
+}
