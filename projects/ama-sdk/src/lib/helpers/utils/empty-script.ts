f12b85172386cdfec0cfa4ea7620fd2de3cb6a3a
Nazareth Jimenez Vela <nazarethjim@gmail.com>
2020-01-24 12:55:45 +0100

    AAE-1566 Fix minor bug in script template (#1131)

--- projects/ama-sdk/src/lib/helpers/utils/empty-script.ts
+++ projects/ama-sdk/src/lib/helpers/utils/empty-script.ts
@@ -64,7 +64,7 @@ export const getEmptyScript = (model) => {
     *            let siteMembershipBodyRequestUpdate = siteMembershipBody.requestUpdate();
     * 
     *  4 - Runtime Commands (ProcessPayloadBuilder and CommandProducer) Example:
-    *          let startProcessInstanceCmd = ProcessPayloadBuilder.start().withProcessDefinitionKey("1af40357-b122-4de1-a031-a71630cbdf33").build();
+    *          let startProcessInstanceCmd = processPayloadBuilder.start().withProcessDefinitionKey("1af40357-b122-4de1-a031-a71630cbdf33").build();
     *          commandProducer.send(startProcessInstanceCmd);
     */`;
 }
