8c966a724aa30bc64296e7ed351ed7e117d0fcd8
Popovics András <popovicsandras@users.noreply.github.com>
2020-02-10 20:13:17 +0100

    Nxify PR fixes (#1197)
    * Fix modeling-ce unit standalone|vscode|root launchers
    
    * Fix modeling-ee unit vscode|root launchers
    
    * Fix collaborators i18n

--- jest.config.js
+++ jest.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 
 module.exports = {
     preset: 'jest-preset-angular',
-    rootDir: process.cwd(),
+    rootDir: path.resolve(__dirname),
     verbose: true,
     testURL: 'http://localhost',
     setupTestFrameworkScriptFile: path.resolve(__dirname, 'jest/jest-setup.ts'),
-    coverageDirectory: '<rootDir>/coverage/modeling-ce',
+    coverageDirectory: '<rootDir>/../../coverage/modeling-ce',
     collectCoverage: true,
     collectCoverageFrom: [
-        '<rootDir>/apps/modeling-ce/src/**/*.ts',
-        '<rootDir>/apps/modeling-ce/projects/**/*.ts',
-        '!<rootDir>/apps/modeling-ce/jest',
-        '!<rootDir>/apps/modeling-ce/src/*.ts',
-        '!<rootDir>/apps/modeling-ce/src/**/*.d.ts',
-        '!<rootDir>/apps/modeling-ce/src/**/index.ts',
-        '!<rootDir>/apps/modeling-ce/projects/**/*.d.ts',
-        '!<rootDir>/apps/modeling-ce/ama-testing/src/e2e',
+        '<rootDir>/src/**/*.ts',
+        '<rootDir>/projects/**/*.ts',
+        '!<rootDir>/jest',
+        '!<rootDir>/src/*.ts',
+        '!<rootDir>/src/**/*.d.ts',
+        '!<rootDir>/src/**/index.ts',
+        '!<rootDir>/projects/**/*.d.ts',
+        '!<rootDir>/ama-testing/src/e2e',
     ],
     roots: [
         path.resolve(__dirname, 'projects'),
@@ -27,7 +27,17 @@ module.exports = {
     ],
     setupFiles: ['core-js/es7/array'],
     transform: {
-        '^.+\\.(ts|html)$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js',
+        '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js',
         '^.+\\.js$': 'babel-jest'
+    },
+    snapshotSerializers: [
+        "jest-preset-angular/AngularSnapshotSerializer.js",
+        "jest-preset-angular/HTMLCommentSerializer.js"
+    ],
+    globals: {
+        "ts-jest": {
+          "tsConfigFile": "<rootDir>/tsconfig.spec.json"
+        },
+        "__TRANSFORM_HTML__": true
     }
 };
