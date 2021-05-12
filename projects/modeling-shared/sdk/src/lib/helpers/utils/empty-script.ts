a15247273eb6c0a278cb1e9c67aac7a861154c1c
Nazareth Jimenez Vela <nazarethjim@gmail.com>
2021-05-12 18:00:16 +0200

    [AAE-4837] - Modify template for empty script (#2580)
    * AAE-4837 Modify template for empty script
    
    * AAE-4837 Modify template for empty script
    
    * AAE-4837 Modify template for empty script
    
    * Minor change
    
    * Improvement

--- projects/modeling-shared/sdk/src/lib/helpers/utils/empty-script.ts
+++ projects/modeling-shared/sdk/src/lib/helpers/utils/empty-script.ts
@@ -29,76 +29,68 @@ export const getEmptyScript = (model) => {
     *  2 - Output Variables Example:
     *          variables.totalCost = cost * (1 + taxes);
     *
-    *  3 - Content APIs Example. The following APIs are currently supported: nodesApi, groupsApi, peopleApi and siteApi.
+    *  3 - Content APIs Example. The following APIs are currently supported: actionsApi, queryApi, searchApi, nodesApi, groupsApi, peopleApi and siteApi.
     *
     *   *** PLEASE NOTE: for some actions (like creating a person or a group) the script runtime client must belong to the ALFRESCO_ADMINISTRATORS group ***
+    * 
+    *   Examples for the different APIs:
+    * 
+    *       3.1.- actionsApi:
+    *           
+    *           Executes an action:
+    *           const actionBodyExec = {"actionDefinitionId":"copy","targetId": "4c4b3c43-f18b-43ff-af84-751f16f1ddfd","params": {"destination-folder": "workspace://SpacesStore/34219f79-66fa-4ebf-b371-118598af898c"}}
+    *           const actionService = new ActionService();
+    *           const result = actionService.actionExec(actionBodyExec);
+    *           variables.id = result.entry.id;
+    * 
+    *       3.2.- queryApi:
+    * 
+    *           Finds nodes:
+    *           const term = "lorem";
+    *           const queryService = new QueryService();
+    *           const result = queryService.findNodes(term);
+    *           variables.name = result.list.entries[0].entry.name;
+    *   
+    *       3.3.- searchApi:
+    * 
+    *           Searches Alfresco:
+    *           const query = {"query": {"query": "cm:title:alfrezco"}}
+    *           const searchService = new SearchService();
+    *           searchService.search(query);
+    * 
+    *       3.4.- nodesApi:
     *
-    *          const parentNodeId = 'daf40357-b177-4de1-a031-a71630cbdfb4';
-    *          const nodeBodyCreate = nodeBody.create();
-    *          nodeBodyCreate.name('test');
-    *          nodeBodyCreate.nodeType('cm:folder');
-    *          nodesApi.createNode(parentNodeId, nodeBodyCreate, true, null, null);
-    *
-    *          const groupBodyCreate = groupBody.create();
-    *          groupBodyCreate.id(variables.groupId);
-    *          groupBodyCreate.displayName(variables.groupName);
-    *          const response = groupsApi.createGroup(groupBodyCreate, null, null);
-    *          if (response.getStatusCode().is2xxSuccessful()) {
-    *              variables.groupId = response.getBody().getEntry().getId();
-    *          }
-    *
-    *          const personBodyCreate = personBody.create();
-    *          personBodyCreate.id(variables.personId);
-    *          personBodyCreate.email(variables.email);
-    *          personBodyCreate.firstName(variables.personFirstName);
-    *          personBodyCreate.lastName(variables.personFirstName);
-    *          personBodyCreate.enabled(true);
-    *          personBodyCreate.password('A1?2c3#4D');
-    *          const response = peopleApi.createPerson(personBodyCreate, null);
-    *          if (response.getStatusCode().is2xxSuccessful()) {
-    *              variables.personId = response.getBody().getEntry().getId();
-    *          }
-    *
-    *          const groupMembershipBodyCreate = groupMembershipBody.create();
-    *          groupMembershipBodyCreate.id(variables.personId);
-    *          groupMembershipBodyCreate.memberType(groupMembershipBody.memberTypePerson());
-    *          groupsApi.createGroupMembership(variables.groupId, groupMembershipBodyCreate, null);
-    *
-    *           const siteBodyCreate = sitesBody.create();
-    *           siteBodyCreate.setTitle(variables.siteName);
-    *           siteBodyCreate.visibility(sitesBody.siteCreateVisibilityPublic());
-    *           const response = sitesApi.createSite(siteBodyCreate, false, false, null);
-    *           if (response.getStatusCode().is2xxSuccessful()) {
-    *               variables.siteId = response.getBody().getEntry().getId();
-    *           }
-    *
-    *      The syntax to create the needed @Body for the differents APIs are:
-    *      3.1 - nodesApi:
-    *            const nodeBodyCreate = nodeBody.create();
-    *            const nodeBodyUpdate = nodeBody.update();
-    *            const nodeBodyLock = nodeBody.lock();
-    *            const nodeBodyMove = nodeBody.move();
-    *
-    *      3.2 - groupsApi:
-    *            const groupBodyCreate = groupBody.create();
-    *            const groupBodyUpdate = groupBody.update();
-    *            const groupMembershipBodyCreate = groupMembershipBody.create();
-    *
-    *      3.3 - peopleApi:
-    *            const personBodyCreate = personBody.create();
-    *            const personBodyUpdate = personBody.update();
-    *            const passwordResetBody = passwordBody.reset();
-    *
-    *      3.4 - siteApi:
-    *            const siteBodyCreate = sitesBody.create();
-    *            const siteBodyUpdate = sitesBody.update();
-    *            const siteMembershipBodyCreate = siteMembershipBody.create();
-    *            const siteMembershipBodyUpdate = siteMembershipBody.update();
-    *            const siteMembershipBodyApproval = siteMembershipBody.approval();
-    *            const siteMembershipBodyRequestCreate = siteMembershipBody.requestCreate();
-    *            const siteMembershipBodyRejection = siteMembershipBody.rejection();
-    *            const siteMembershipBodyRequestUpdate = siteMembershipBody.requestUpdate();
-    *
+    *           Creates a node:
+    *           const parentNodeId = 'daf40357-b177-4de1-a031-a71630cbdfb4';
+    *           const nodeBodyCreate = {name:"test2", nodeType: "cm:folder"};
+    *           const nodeService = new NodeService();
+    *           const node = nodeService.createNode(parentNodeId, nodeBodyCreate);
+    *           variables.id = node.entry.id;
+    * 
+    *       3.5.- groupsApi:
+    * 
+    *           Creates a group:
+    *           const groupBodyCreate = {"id":variables.groupId, "displayName": variables.groupName};
+    *           const groupService = new GroupService();
+    *           const group = groupService.createGroup(groupBodyCreate);
+    *           variables.displayName = group.entry.displayName;
+    * 
+    *       3.6.- peopleApi:
+    * 
+    *           Creates a person:
+    *           const personBodyCreate = {"id": "abeecher","firstName": "Alice","lastName": "Beecher","displayName": "Alice Beecher","email": "abeecher@example.com","password": "secret","properties":{"my:property": "The value"}}
+    *           const peopleService = new PeopleService();
+    *           const people = peopleService.createPerson(personBodyCreate);
+    *           variables.email = people.entry.email;
+    * 
+    *       3.7.- sitesApi:
+    * 
+    *           Creates a site:
+    *           const siteBodyCreate = {"title": "Marketing","visibility": "PUBLIC"}
+    *           const siteService = new SiteService();
+    *           const site = siteService.createSite(siteBodyCreate);
+    *           variables.title = site.entry.title;
+    * 
     *  4 - Runtime Commands (ProcessPayloadBuilder and CommandProducer) Example:
     *          const startProcessInstanceCmd = processPayloadBuilder.start().withProcessDefinitionKey('1af40357-b122-4de1-a031-a71630cbdf33').build();
     *          commandProducer.send(startProcessInstanceCmd);
