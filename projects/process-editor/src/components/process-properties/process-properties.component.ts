0151088d2d1ea00572c8ef0055633866b368f2fc
Rafal Szmit <40626242+rafalszmit@users.noreply.github.com>
2022-05-26 16:03:56 +0200

    [AAE-8778] Add process version to properties panel (#3834)
    Co-authored-by: Sushmitha_Vk <sushmitha.v@muraai.com>

--- projects/process-editor/src/components/process-properties/process-properties.component.ts
+++ projects/process-editor/src/components/process-properties/process-properties.component.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { Component, OnInit, OnDestroy, Inject, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, ChangeDetectionStrategy, ViewEncapsulation, Input } from '@angular/core';
 import { CardViewItem, CardViewUpdateService, UpdateNotification, CardItemTypeService } from '@alfresco/adf-core';
 import { Store } from '@ngrx/store';
 import { selectSelectedElement } from '../../store/process-editor.selectors';
@@ -40,6 +40,8 @@ import { SelectedProcessElement } from '../../store/process-editor.state';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProcessPropertiesComponent implements OnInit, OnDestroy {
+    @Input()
+    processVersion: string;
     onDestroy$: Subject<void> = new Subject<void>();
     properties$: Observable<CardViewItem[]>;
 
