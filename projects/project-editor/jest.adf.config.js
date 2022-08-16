eab022d6972d06aa7ff1f90ec54be9fad55c4e9c
Popovics András <popovicsandras@users.noreply.github.com>
2022-08-16 12:47:48 +0200

    [ci:force] Fix ADF jest configs (#4271)

--- projects/project-editor/jest.adf.config.js
+++ projects/project-editor/jest.adf.config.js
@@ -1,4 +1,5 @@
+const { resolve } = require('path');
 const config = require('./jest.config');
 const { overrideTsConfig } = require('../adf-candidates/testing/jest/jest-utils');
 
-module.exports = overrideTsConfig(config, "<rootDir>/tsconfig.spec.adf.json");
+module.exports = overrideTsConfig(config, resolve(__dirname, 'tsconfig.spec.adf.json'));
